18bf28a05106da3a61cc38a34bc78a09ff740166
jv <jv.lucio@unesp.br>
2025-07-31 12:36:27 -0300

    commit 31/7

--- testec2.c
+++ testec2.c
@@ -1,5 +1,31 @@
-int main(int argc, char const *argv[])
-{
-    printf("hello world!! acertei?");
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d", &x);
+
+    for (int i = 0; i < x; i++) {
+        double a, b;
+        char op;
+
+        // Lê na mesma linha: número, operador e outro número
+        scanf("%lf %c %lf", &a, &op, &b);
+
+        if (op == '+') {
+            printf("%.2lf\n", a + b);
+        } else if (op == '-') {
+            printf("%.2lf\n", a - b);
+        } else if (op == '*') {
+            printf("%.2lf\n", a * b);
+        } else if (op == '/') {
+            if (b != 0)
+                printf("%.2lf\n", a / b);
+            else
+                printf("Erro: divisão por zero\n");
+        } else {
+            printf("Operador inválido\n");
+        }
+    }
+
     return 0;
 }
